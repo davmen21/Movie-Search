a59682bc7cdb55b670d993c485531d6a9cb75199
David <86984492+davmen21@users.noreply.github.com>
2023-11-28 20:38:49 -0500

    Main FXML Update

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,36 @@
 import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
+
 import javafx.stage.Stage;
 
 
-public class Main extends Application{
-    public static void main(String[] args){
+public class Main extends Application
+{
+
+    public static void main(String[] args)
+    {
         launch(args);
     }
+    
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Movie Search");
-        
-        StackPane layout = new StackPane();
-
-        Scene scene = new Scene(layout, 500, 300);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception 
+    {
+      //Load FXML View
+      Parent root = FXMLLoader.load(getClass().getResource("/views/view.fxml"));
+      
+      //Create and display Scene and Stage
+      Scene scene = new Scene(root);
+      primaryStage.setTitle("Movie Search");   
+      primaryStage.setScene(scene);
+      primaryStage.show();
     }
+    
+
 }
