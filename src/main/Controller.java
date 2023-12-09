fd4d05158585f1cd1f343fefd59565f8e19d5349
David <86984492+davmen21@users.noreply.github.com>
2023-12-08 23:57:25 -0500

    Created changeViewMode and initialize
    Added initialize method for user preferences on Dark/Light mode.

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,5 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
@@ -13,19 +15,24 @@ import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import java.io.*;
 import java.util.Scanner;
+import java.util.prefs.Preferences;
+import java.util.ResourceBundle;
 import java.net.URL;
 import java.net.MalformedURLException;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class Controller{
+public class Controller implements Initializable{
 
     @FXML
     private TextField searchInput;
     private MenuButton sortId;
     private Button submit;
     private TabPane tabPane;
+    
+    @FXML
+    private AnchorPane anchorPane;
     
     @FXML
     private Tab resultsTab;
@@ -42,15 +49,24 @@ public class Controller{
     @FXML
     private TableColumn plotColumn;
     
-    String input;
-    String sort;
+    @FXML
+    private Button viewToggle;
+    
+    private String input;
+    private String sort;
+    private enum Mode {DARKMODE, LIGHTMODE};
+    private Mode mode;
+    public static final String VIEWMODE = "view_mode_key";
+    
+    
     
    /**Search Button action event calls Search.searchByTitle method and passes search results to the table columns
    @param event: ActionEvent of button click
    **/
    
    @FXML
-   void handleButton(ActionEvent event) throws IOException{
+   protected void handleButton(ActionEvent event) throws IOException
+   {
       //Get text field input
       input = searchInput.getText();
       
@@ -64,6 +80,33 @@ public class Controller{
       resultsTable.setItems(movieResults);
       
    }
+   
+   /** Change view styling based on view mode selected and
+   update user preferences for view mode
+   **/
+   @FXML   
+   protected void changeViewMode(ActionEvent event)
+   {
+      if(this.mode == Mode.DARKMODE)
+      {
+         this.mode = Mode.LIGHTMODE;
+         viewToggle.setText("Dark Mode");
+         viewToggle.setStyle("");
+         anchorPane.setStyle("");
+         
+      } 
+      else
+      {
+         this.mode = Mode.DARKMODE;
+         viewToggle.setText("Light Mode");
+         viewToggle.setStyle("-fx-background-color: #707070;");
+         anchorPane.setStyle("-fx-background-color: #595959;");
+      }
+      
+      Preferences p = Preferences.userNodeForPackage(Controller.class);
+      p.put(VIEWMODE, this.mode.toString());
+   }
+   
    
    //Sort MenuItem action events
    @FXML
@@ -84,6 +127,27 @@ public class Controller{
    void addToWatchlist(ActionEvent event){
       
    
+   }
+   
+   /**initialize app with user preferences for 
+   Dark mode and Watchlist
+   **/
+   @Override
+   public void initialize(URL location, ResourceBundle resources)
+   {  
+      Preferences p = Preferences.userNodeForPackage(Controller.class);
+      this.mode = Mode.valueOf( p.get(VIEWMODE, Mode.LIGHTMODE.toString()));
+      
+      if(this.mode == Mode.DARKMODE)
+      {
+         viewToggle.setText("Light Mode");
+         viewToggle.setStyle("-fx-background-color: #707070;");
+         anchorPane.setStyle("-fx-background-color: #595959;");
+      } else 
+      {
+         viewToggle.setText("Dark Mode");
+
+      }
    }
 
 }
